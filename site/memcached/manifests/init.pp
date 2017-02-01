5367a22ebb6ccc6238fd5c75deef667521e3c33a
benjFBL <ben.johnston@fblfinancial.com>
2017-02-01 15:43:40 -0600

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -15,6 +15,6 @@ class memcached {
  service { 'memcached':
   ensure => running,
   enable => true,
-  subscribe => File['/ect/sysconfig/memcached'],
+  subscribe => File['/etc/sysconfig/memcached'],
  }
 } 
