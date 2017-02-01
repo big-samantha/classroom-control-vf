ba98772f99a6a0e853e43dfa20b48cbfee6bc148
mkhafeez <mhafeez@edge.com>
2017-02-01 09:32:41 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+file { '/etc/motd':
+  ensure  => kash,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0755',
+  content => "what have you done kash",
 }
