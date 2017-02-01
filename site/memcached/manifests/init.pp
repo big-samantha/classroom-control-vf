1a57d1db12e06938c3c5bcd0183134e4d91362e9
Zee Alexander <zee@zee.space>
2017-02-01 13:14:00 -0800

    add memcached main class

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,3 +1,21 @@
 class memcached {
-
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  file { 'memcached-sysconfig':
+    ensure  => file,
+    path    => '/etc/sysconfig/memcached',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached-sysconfig',
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['memcached-sysconfig'],
+  }
 }
