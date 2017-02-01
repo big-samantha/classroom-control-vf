b5e644ff1460350ae7e35fe2b06d161f8a79e3be
David Crouse <david@crouse.us>
2017-02-01 10:11:43 -0600

    Exercise 7.2:

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,12 +44,17 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-  ensure => file,  
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => 'Dave Was here',
+#  file { '/etc/motd':
+#  ensure => file,  
+#  owner => 'root',
+#  group => 'root',
+#  mode => '0644',
+#  content => 'Dave Was here',
+#}
+
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+path => '/usr/bin:/usr/local/bin',
+creates => '/etc/motd',
 }
 
 
