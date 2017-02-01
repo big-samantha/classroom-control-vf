953f71b7c0f38dfe2acb5bed66aa0af953398305
LearningPuppet <hdao02@gmail.com>
2017-02-01 14:03:10 -0500

    Adding users lab 9.2
      include examples::users

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,9 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  #lab 9.2 
+  include examples::users
+  
   #lab 7.1
   file { '/etc/motd':
     ensure => file,
@@ -57,4 +60,5 @@ node default {
       path => '/usr/bin:/usr/local/bin',
       #creates => '/etc/motd',
      }
+   
 }
