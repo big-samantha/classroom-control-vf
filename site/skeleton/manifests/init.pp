f32b87496edff6f92a4fdf63d8373d4890857594
cdann888 <carol.d.131719@gmail.com>
2017-02-01 14:58:37 -0500

    add class skeleton

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,17 @@
 class skeleton {
+
+  file {'/etc/skel':
+    ensure => 'directory',
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+    }
+    
   file {'/etc/skel/.bashrc':
     ensure => file,
-    source => '/etc/skel',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///etc/skeleton/bashrc',
   }
 }
