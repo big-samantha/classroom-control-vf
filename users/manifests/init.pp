0c43eeaf0811d5f5eeab2ec072ec2f9572ae9cb5
zyx989 <jbuckowing@hotmail.com>
2017-02-01 12:28:00 -0600

    Test again
    whatever again

--- users/manifests/init.pp
+++ users/manifests/init.pp
@@ -1,7 +1,6 @@
 class users {
   user {'fundamentals':
   ensure => present,
-# password => 'puppet8#labs' # Windows requires a plain text password
-# groups => ['Users'],  # Display Windows Control Panel
+
   }
 }
