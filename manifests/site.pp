226f21ce1a65f27106c85926e921c850c69e1c1a
Val Baranov <val.baranov@duke.edu>
2017-02-01 11:14:10 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,12 +44,17 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 
-file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "MOTD is a must\n",
+#file { '/etc/motd':
+#  ensure  => file,
+#  owner   => 'root',
+#  group   => 'root',
+#  mode    => '0644',
+#  content => "MOTD is a must\n",
+#}
+
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+path => '/usr/bin:/usr/local/bin',
+creates => '/etc/motd',
 }
 }
 
