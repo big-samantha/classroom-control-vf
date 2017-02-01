90bfc7b4eab5908c21f87846ffcd67d0aa5567aa
Val Baranov <val.baranov@duke.edu>
2017-02-01 10:33:50 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,14 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "MOTD is a must\n",
 }
+}
+
+
