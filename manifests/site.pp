8c1d41db3222b9fc636b1b690014394e3b592804
Val Baranov <val.baranov@duke.edu>
2017-02-02 10:51:24 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,12 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+
   include role::classroom
 
 #file { '/etc/motd':
